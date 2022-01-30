c82876a7a4f995e72ea00cf2077b33a4743c2611
purnima05-ux <Umeshjayswal2013@gmail.com>
2022-01-30 18:06:58 +0545

    Swaping program added

---
+++ swaping.java
@@ -0,0 +1,18 @@
+
+public class SwapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100; int b=200;
+		System.out.println("Before Swapping");
+		System.out.println("a :"+a+ " b:"+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After Swapping");
+		System.out.println("a :"+a+ " b:"+b);
+
+
+	}
+
+}
